007d3ab41780d0595c0f1236000dab8946fbf842
Erik Schnetter <schnetter@gmail.com>
2018-11-25 11:26:54 -0500

    Enable MacOS SDK on Travis OSX platform

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -35,6 +35,7 @@ platforms_osx = [
 if ENV["TRAVIS_OS_NAME"] == "linux"
     platforms = platforms_linux
 elseif ENV["TRAVIS_OS_NAME"] == "osx"
+    ENV["BINARYBUILDER_AUTOMATIC_APPLE"] = "1"
     platforms = platforms_osx
 else
     platforms = [platforms_linux; platforms_osx]
